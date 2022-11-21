e65d148006c1a219635485aa8e9ddeda58290570
wonggamto <wonggamto@163.com>
2022-11-22 04:39:45 +0800

    使用 tsx 代替 template

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   // github 部署地址
   // base:'/mangosteen-pages-1/dist/',
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    vueJsx({
+      transformOn:true,
+      mergeProps:true
+    })
+  ]
 })
