555f3636f05a098246078a99def1b2843bdea6ef
wonggamto <wonggamto@163.com>
2022-11-22 03:35:21 +0800

    reset base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/mangosteen-pages-1/dist/',
+  // github 部署地址
+  // base:'/mangosteen-pages-1/dist/',
   plugins: [vue()]
 })
