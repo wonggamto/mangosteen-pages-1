592e584e59205c7445cad13a9d90bb6e640ff4b5
wonggamto <wonggamto@163.com>
2022-11-22 16:59:59 +0800

    创建欢迎界面的页面

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,5 +1,23 @@
-import { Bar , Foo} from './views/Bar'
-export const routes = [
+import { RouteRecordRaw } from 'vue-router'
+import { First } from '../components/welcome/first'
+import { Forth } from '../components/welcome/forth'
+import { Second } from '../components/welcome/second'
+import { Third } from '../components/welcome/third'
+import { Bar } from '../views/Bar'
+import {Foo} from '../views/Foo'
+import { Welcome } from '../views/Welcome'
+
+
+export const routes: RouteRecordRaw[] = [
     {path:'/',component:Foo},
-    {path:'/about',component:Bar}
+    {path:'/about',component:Bar},
+    {
+        path:'/welcome',
+        component:Welcome,
+        children:[
+            {path:'1',component:First},
+            {path:'2',component:Second},
+            {path:'3',component:Third},
+            {path:'4',component:Forth},
+        ]}
 ]
